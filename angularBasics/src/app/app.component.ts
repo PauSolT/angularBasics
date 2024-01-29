9466dee3cf4eb0bac618a557306c63a5f3630ef5
alexmrtc <alexmrtc@gmail.com>
2024-01-29 12:46:00 +0100

    Component Communication with @Output

--- angularBasics/src/app/app.component.ts
+++ angularBasics/src/app/app.component.ts
@@ -2,16 +2,21 @@ import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { UserComponent } from './user/user.component';
+import { ChildComponent } from './child/child.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, UserComponent],
+  imports: [CommonModule, RouterOutlet, UserComponent, ChildComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  
+  items = new Array();
+
+  addItem(item: string) {
+    this.items.push(item);
+  }
 }
 
 
