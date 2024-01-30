9bb631fd4533fe63a3d354ad0304ef2b88174110
pau <pausoltor@gmail.com>
2024-01-30 10:36:53 +0100

    Deferrable Views

--- angularBasics/src/app/app.component.ts
+++ angularBasics/src/app/app.component.ts
@@ -3,20 +3,17 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { UserComponent } from './user/user.component';
 import { ChildComponent } from './child/child.component';
+import { CommentsComponent } from './comments/comments.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, UserComponent, ChildComponent],
+  imports: [CommonModule, RouterOutlet, UserComponent, ChildComponent, CommentsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  items = new Array();
-
-  addItem(item: string) {
-    this.items.push(item);
-  }
+ 
 }
 
 
