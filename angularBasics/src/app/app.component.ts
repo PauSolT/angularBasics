7cc402fbbe192a24eaa3d0c7a94367c1fa3de7d7
alexmrtc <alexmrtc@gmail.com>
2024-01-30 11:03:03 +0100

    Enable, define and link routes

--- angularBasics/src/app/app.component.ts
+++ angularBasics/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 import { UserComponent } from './user/user.component';
 import { ChildComponent } from './child/child.component';
 import { CommentsComponent } from './comments/comments.component';
@@ -8,12 +8,12 @@ import { CommentsComponent } from './comments/comments.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, UserComponent, ChildComponent, CommentsComponent],
+  imports: [CommonModule, RouterOutlet, RouterLink, UserComponent, ChildComponent, CommentsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
- 
+
 }
 
 
