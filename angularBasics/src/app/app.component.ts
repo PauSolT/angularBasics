81afab630d778e034b8cb4e610620760ff7514f5
pau <pausoltor@gmail.com>
2024-02-01 11:54:41 +0100

    Reactive Forms

--- angularBasics/src/app/app.component.ts
+++ angularBasics/src/app/app.component.ts
@@ -4,16 +4,26 @@ import { RouterOutlet, RouterLink } from '@angular/router';
 import { UserComponent } from './user/user.component';
 import { ChildComponent } from './child/child.component';
 import { CommentsComponent } from './comments/comments.component';
+import {ReactiveFormsModule, FormControl, FormGroup } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, UserComponent, ChildComponent, CommentsComponent],
+  imports: [CommonModule, RouterOutlet, RouterLink, UserComponent, ChildComponent, CommentsComponent,ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-
+  profileForm = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl(''),
+  });
+  handleSubmit() {
+    alert(
+      this.profileForm.value.name + ' | ' + this.profileForm.value.email
+    );
+  }
 }
 
 
